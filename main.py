9e3535375ee097e86092357e668a2826c2be89ae
Morteza Salarkia <msalarkia3@gmail.com>
2020-05-24 16:29:50 +0430

    examples added

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from datastructures.queue.queue import Queue
+from datastructures.stack.stack import Stack
 
 if __name__ == "__main__":
+    # testing stack
+    stack = Stack()
+    stack.push(10)
+    stack.push(20)
+    stack.push(30)
+
+    print(stack)
+
+    print(stack.pop())
+    print(stack.pop())
+    print(stack)
+
+    stack.pop()
+    try:
+        stack.pop() # should raise EmptyStackException 
+    except Exception as e:
+        print(e)
+    
+    # testing Queue
     queue = Queue()
 
     queue.enqueue(10)
@@ -10,4 +30,7 @@ if __name__ == "__main__":
     print(queue.dequeue())
     print(queue.dequeue())
     print(queue.dequeue())
-    print(queue.dequeue())
+    try:
+        queue.dequeue()
+    except Exception as e:
+        print(e)
