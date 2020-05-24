58b23bdcd53e4aaf85d23ce8fda26b210bc8d270
Morteza Salarkia <msalarkia3@gmail.com>
2020-05-24 16:24:53 +0430

    removed comment

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-print('__file__={0:<35} | __name__={1:<20} | __package__={2:<20}'.format(__file__,__name__,str(__package__)))
-
 from datastructures.queue.queue import Queue
 
 if __name__ == "__main__":
