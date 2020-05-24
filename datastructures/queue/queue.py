c2d08a4465d273d0804548b9855f12a43bfad492
Morteza Salarkia <msalarkia3@gmail.com>
2020-05-24 16:23:51 +0430

    removed execution

--- datastructures/queue/queue.py
+++ datastructures/queue/queue.py
@@ -35,15 +35,3 @@ class Queue:
         
         return self._dequeue_stack.peek()
 
-
-if __name__ == "__main__":
-    queue = Queue()
-
-    queue.enqueue(10)
-    queue.enqueue(20)
-    queue.enqueue(30)
-
-    print(queue.dequeue())
-    print(queue.dequeue())
-    print(queue.dequeue())
-    print(queue.dequeue())
